b9f405be2899f7bf64bea6372d925e2703f37700
Brenno Lemos <dev@brenno.codes>
2025-05-09 21:49:54 -0300

    fix: swap Span::source_file -> Span::file rust-lang/rust#54725 rust-lang/rust#139903

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,30 @@
 //! A simple proc macro to generate a const list of filenames and optionally apply a macro to each.
-//! 
+//!
 //! ```rust
 //! use std::fs::canonicalize;
-//! 
+//!
 //! use list_files_macro::list_files;
-//! 
+//!
 //! fn get_full_path(path: &str) -> String {
 //! 	canonicalize(path).unwrap().into_os_string().into_string().unwrap()
 //! }
-//! 
+//!
 //! const FILENAMES: [&'static str; 3] = list_files!("../tests/files/*.rs");
-//! 
+//!
 //! assert_eq!(FILENAMES, [
 //! 	"tests/files/a.rs",
 //! 	"tests/files/b.rs",
 //! 	"tests/files/c.rs",
 //! ].map(get_full_path));
-//! 
+//!
 //! const CONTENTS: [&'static str; 3] = list_files!(include_str, "../tests/files/*.rs");
-//! 
+//!
 //! assert_eq!(CONTENTS[0], r#"
 //! pub fn run() -> &'static str {
 //! 	"A"
 //! }
 //! "#);
-//! 
+//!
 //! macro_rules! run_file {
 //! 	($x:expr) => {
 //! 		{
@@ -34,23 +34,22 @@
 //! 		}
 //! 	};
 //! }
-//! 
+//!
 //! let results = list_files!(run_file, "../tests/files/*.rs");
-//! 
+//!
 //! assert_eq!(results, [
 //! 	"A",
 //! 	"B",
 //! 	"C",
 //! ]);
 //! ```
-//! 
+//!
 //! To use this, add it as a dependency to your Cargo.toml:
 //! ```toml
 //! [dependencies]
 //! list_files_macro = "^0.1.0"
 //! ```
 
-#![feature(let_else)]
 #![feature(proc_macro_span)]
 
 #![doc(html_root_url = "https://docs.rs/list_files_macro/0.1.0")]
@@ -73,23 +72,23 @@ pub fn list_files(input: TokenStream) -> TokenStream {
 		[Expr::Lit(ExprLit { lit: Lit::Str(path), .. })] => (path.value(), None),
 		_ => panic!("Usage: list_files!(\"path/to/dir\") | list_files!(handler_macro, \"path/to/dir\")"),
 	};
-	
+
 	// Resolve directory
 	let absolute_path =
 		if path.starts_with(".") {
-			let source_path = Span::call_site().source_file().path();
+			let source_path: std::path::PathBuf = Span::call_site().file().into();
 			source_path.parent().unwrap().join(&path).into_os_string().into_string().unwrap()
 		} else {
 			path
 		};
-	
+
 	// Generate list of literal string paths
 	let files = glob(&absolute_path).unwrap();
 	let file_literals = files.map(|file| {
 		let file = file.unwrap().canonicalize().unwrap().into_os_string().into_string().unwrap();
 		quote!(#file)
 	});
-	
+
 	// Run handlers if required
 	if let Some(handler) = handler {
 		quote!([#(#handler!(#file_literals)),*])
